79cd59c83fd05fcb52d542bf17f57943cb1b9d93
apipkurniawan <davitaja92@gmail.com>
2021-11-10 12:13:00 +0700

    complete pwa app

---
+++ dist/precache-manifest.b45b3f6382368a3aa93931ac62192b82.js
@@ -0,0 +1,10 @@
+self.__precacheManifest = [
+  {
+    "revision": "67877176dbf5f73fe5fe",
+    "url": "app.js"
+  },
+  {
+    "revision": "b9d6ad5f2e252422cb10b6036d21c0a2",
+    "url": "index.html"
+  }
+];
