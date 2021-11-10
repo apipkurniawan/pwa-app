ba19c554fdfc4feace3ef02cfc0fb75c8ce6ecd8
apipkurniawan <davitaja92@gmail.com>
2021-11-10 12:31:23 +0700

    complete pwa app

---
+++ src/src-sw.js
@@ -0,0 +1,9 @@
+console.log(`hello from service worker`);
+
+workbox.routing.registerRoute(
+  /https:\/\/jsonplaceholder\.typicode\.com/,
+  workbox.strategies.networkFirst()
+);
+
+// workbox.skipWaiting();
+workbox.precaching.precacheAndRoute(self.__precacheManifest);
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const WorkboxWebpackPlugin = require("workbox-webpack-plugin");
+
+module.exports = {
+  mode: "production",
+  entry: "./src/app.js",
+  output: {
+    filename: "app.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new CopyWebpackPlugin([
+      {
+        from: "./src/index.html",
+        to: "index.html",
+      },
+      {
+        from: "./src/manifest.json",
+        to: "manifest.json",
+      },
+      {
+        from: "./src/images/",
+        to: "images",
+      },
+    ]),
+    new WorkboxWebpackPlugin.InjectManifest({
+      swSrc: "./src/src-sw.js",
+      swDest: "sw.js",
+    }),
+  ],
+};
